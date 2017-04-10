55e263a568877ae04139124cedf1023a5660e1e0
Nero Zuo <502350123@qq.com>
2017-04-11 00:22:44 +0800

    not finish task pc command

--- KW01-FFD-ex1/09_MQXLite/app/task_pc_command.c
+++ KW01-FFD-ex1/09_MQXLite/app/task_pc_command.c
@@ -1,5 +1,40 @@
 #include "01_app_include.h"    //应用任务公共头文件
 
-void task_pc_command(uint32_t initial_data){
+#define PC_COM_MSG_SIZE 16
 
+typedef enum PC_COM_TYPE {
+    PC_COM_NODES  = 'n',
+    PC_COM_TEMP = 't'  // data[1] 代表 发送的目的地
+} PC_COM_TYPE;
+
+void task_pc_command(uint32_t initial_data){
+    _mqx_uint pc_com_msg[PC_COM_MSG_SIZE];
+    uint_8 count = 0;
+    int i = 0;
+    while (TRUE) {
+        _lwmsgq_receive((pointer)pccommand_queue,pc_com_msg,LWMSGQ_RECEIVE_BLOCK_ON_EMPTY,0,0); //等待pc_command
+        char *data = pc_com_msg;
+        switch (data[0]) {
+            case PC_COM_NODES:        // all nodes of 
+                while (rfdAddrs[count] != 0) { count++; }
+
+                char str[64] = "Have x nodes "
+                str[5] = count + '0';
+                for (i = 13; i < 13+count*2; i+=2) {
+                    str[i] = rfdAddrs[i] + '0';
+                    str[i+1] = ' ';
+                }
+
+                uart_sendN(UART_0, 13+count*2, str);
+
+                break;
+            case PC_COM_TEMP:
+                char des = data[1];
+                WPSendData("a", 1, NZP_REGISTER_Success, 0xff, 0);
+                break;
+            default:
+                break;
+        }
+
+    }
 }
