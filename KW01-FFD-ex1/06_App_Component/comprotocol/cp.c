1d9ef6239ef170da14b373f49cded71039c8031a
Chaos <gamijin0@gmail.com>
2017-04-13 21:23:58 +0800

    add comprotocol for FFD

---
+++ KW01-FFD-ex1/06_App_Component/comprotocol/cp.c
@@ -0,0 +1,43 @@
+/*
+ * cp.c
+ *
+ *  Created on: 2017年4月13日
+ *      Author: 49738
+ */
+
+#include "cp.h"
+#include "uart.h"
+
+// 发送节点注册成功消息
+void sendNodeReisterSuccessInfo(char nodeAddr) {
+    char dataToSend[2] = {RegisterSuccess, nodeAddr};
+    char dataLength = 2;
+    uart_sendN(UART_0,dataLength,dataToSend);
+}
+
+// 发送在线节点信息
+void sendNodeStatus(char numOfNodes, char* nodesAddrs) {
+    char dataToSend[2+numOfNodes];
+    int i = 0;
+    dataToSend[0] = NodeStatus;
+    dataToSend[1] = numOfNodes;
+    for (i = 0; i < numOfNodes; i++) {
+        dataToSend[i+2] = nodesAddrs[i];
+    }
+    char dataLength = 2 + numOfNodes;
+   	uart_sendN(UART_0,dataLength,dataToSend);
+}
+
+// 发送温度信息
+void sendNodeTempInfo(char nodeAddr, char* tempFloat) {
+    char dataLength = 2+4;
+    int i = 0;
+    char dataToSend[dataLength];
+    dataToSend[0] = TempInfo;
+    dataToSend[1] = nodeAddr;
+    for (i = 0; i < 4; i++) {
+        dataToSend[i+2] = tempFloat[i];
+    }
+    uart_sendN(UART_0,dataLength,dataToSend);
+}
+
---
+++ KW01-FFD-ex1/06_App_Component/comprotocol/cp.h
@@ -0,0 +1,29 @@
+/*
+ * cp.h
+ *
+ *  Created on: 2017年4月13日
+ *      Author: 49738
+ */
+
+#ifndef CP_H
+#define CP_H
+
+#include "common.h"
+
+typedef enum FFDDataType {
+    RegisterSuccess = 's',  // s|node address
+    NodeStatus = 'n',       // n|num of nodes|node0|node1...
+    TempInfo   = 't'        // t|node address|temp(float)
+} FFDDataType;
+
+
+// 发送节点注册成功消息
+void sendNodeReisterSuccessInfo(char nodeAddr) ;
+
+// 发送在线节点信息
+void sendNodeStatus(char numOfNodes, char* nodesAddrs) ;
+
+// 发送温度信息
+void sendNodeTempInfo(char nodeAddr, char* tempFloat) ;
+
+#endif
