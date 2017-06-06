2643bb1b2ba46b982fc72975877c79a8bfdb7bba
chaos <497386856@qq.com>
2017-06-06 12:56:44 +0800

    add help

---
+++ CSharp-PC-ex1/02-Form/TrackBarForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SerialPort._02_Form
+{
+    partial class 小灯闪烁频率控制
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(115, 71);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(510, 56);
+            this.trackBar1.SmallChange = 25;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // 小灯闪烁频率控制
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 214);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "小灯闪烁频率控制";
+            this.Text = "小灯闪烁频率";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
---
+++ CSharp-PC-ex1/02-Form/TrackBarForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SerialPort._02_Form
+{
+    public partial class 小灯闪烁频率控制 : Form
+    {
+        private byte addr;
+        private FrmSCI f;
+        public 小灯闪烁频率控制(byte a,FrmSCI f)
+        {
+            InitializeComponent();
+            this.addr = a;
+            this.f = f;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.f.sendLightControlMessage(this.addr, (byte)this.trackBar1.Value, sender, e);
+        }
+    }
+}
